5d9c303a752a94ef3490e20ad7fb5e2ebb9819db
NicolasRodriguezUribe <nicolas.rodriguez@urjc.es>
2022-01-30 20:01:35 +0100

    First example
    Example1 with Java 17

---
+++ example1/example1/src/main/java/es/dws/example1/GreetingController.java
@@ -0,0 +1,19 @@
+package es.dws.example1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+
+        model.addAttribute("name", "World");
+
+        return "greeting_template";
+    }
+}
+
+
