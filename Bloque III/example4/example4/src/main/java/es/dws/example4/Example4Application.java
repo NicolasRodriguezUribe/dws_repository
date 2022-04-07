014f7702f0f3e2333e697cca96936e2a05a6da2d
NicolasRodriguezUribe <nicolas.rodriguez@urjc.es>
2022-04-07 23:40:21 +0200

    Database example

---
+++ Bloque III/example4/example4/src/main/java/es/dws/example4/Example4Application.java
@@ -0,0 +1,13 @@
+package es.dws.example4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example4Application.class, args);
+	}
+
+}
